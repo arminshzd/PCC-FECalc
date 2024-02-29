0a47cec4c5297d14378c46fc54ac07792b53a63e
Armin Shayesteh Zadeh <arminsh@uchicago.edu>
2024-02-29 09:45:14 -0600

    Added GPyTorchModel inheritence

--- MFMOBO/gskgpr.py
+++ MFMOBO/gskgpr.py
@@ -2,15 +2,15 @@ import matplotlib.pyplot as plt
 import torch
 import gpytorch
 from gskernel import GenericStringKernel
-#from gskernel_cpp import GenericStringKernel
+from botorch.models.gpytorch import GPyTorchModel
 from tqdm import tqdm
 
 
-class GaussianStringKernelGP(gpytorch.models.ExactGP):
+class GaussianStringKernelGP(gpytorch.models.ExactGP, GPyTorchModel):
     def __init__(self, train_x, train_y, likelihood, translator, **kwargs):
         super().__init__(train_x, train_y, likelihood)
         self.mean_module = gpytorch.means.ConstantMean()
-        self.covar_module = GenericStringKernel(translator, **kwargs)#gpytorch.kernels.RBFKernel()
+        self.covar_module = GenericStringKernel(translator, **kwargs)
 
     def forward(self, x):
         mean_x = self.mean_module(x)
